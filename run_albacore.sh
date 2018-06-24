d8bade49d44277b64fac0b6f2143d2c1e81af7be
Chunyu Zhao <zhaocy.dut@gmail.com>
2018-06-24 19:01:25 -0400

    Update run_albacore.sh

--- run_albacore.sh
+++ run_albacore.sh
@@ -4,3 +4,5 @@ raw_fast5_fp=/mnt/isilon/microbiome/incoming/nanopore/run14_cdiff_t15_t18_t19_t2
 basecalled_fast5_fp=/scr1/users/zhaoc1/projects/basecalled_run14_20180329
 read_fast5_basecaller.py --flowcell FLO-MIN106 --kit SQK-RBK001 --barcoding --output_format fast5,fastq \
 	--worker_threads 8 --recursive --input $raw_fast5_fp --save_path $basecalled_fast5_fp
+
+#TODO: check out the chunk size Gus mentioned
